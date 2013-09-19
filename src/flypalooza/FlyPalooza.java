a3104a3bef846027471e090a9095303ea2418e79
Eduardo Ismael Garcia Perez <eduardo78d@gmail.com>
2013-09-19 18:43:37 -0500

    Version primitiva

---
+++ src/flypalooza/FlyPalooza.java
@@ -0,0 +1,23 @@
+
+package flypalooza;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class FlyPalooza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JFrame ventana = new JFrame();
+       Screen screen = new Screen();
+       ventana.add(screen);
+       ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       ventana.setSize(600,700);
+       ventana.setVisible(true);
+    }
+}
